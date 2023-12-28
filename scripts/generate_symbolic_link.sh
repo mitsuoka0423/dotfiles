cc55286e32586c2a1253389bc9f011b0e9a8c3e2
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2023-12-29 08:01:40 +0900

    add

--- scripts/generate_symbolic_link.sh
+++ scripts/generate_symbolic_link.sh
@@ -1,11 +1,13 @@
 # THIS_DIR="$(dirname $0)"
-THIS_DIR='/Users/nix/ghq/github.com/mitsuoka0423/dotfiles'
+THIS_DIR="/Users/$(id -un)/ghq/github.com/mitsuoka0423/dotfiles"
 echo $THIS_DIR
-cd "$THIS_DIR"
+cd $THIS_DIR
 
 ln -snfv "$THIS_DIR/.Brewfile" "$HOME"
 ln -snfv "$THIS_DIR/.Brewfile.lock.json" "$HOME"
 ln -snfv "$THIS_DIR/Makefile" "$HOME"
 ln -snfv "$THIS_DIR/home-manager/home.nix" "$HOME/.config/home-manager"
+ln -snfv "$THIS_DIR/karabiner/karabiner.json" "$HOME/.config/karabiner"
+ln -snfv "$THIS_DIR/nix/nix.conf" "$HOME/.config/nix"
 
 cd ~
