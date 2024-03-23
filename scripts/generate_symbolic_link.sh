847fb4688cdd7188078d6dc8f608d1aed5c86377
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2024-03-23 14:54:34 +0900

    feat: シンボリックリンクに変更

--- scripts/generate_symbolic_link.sh
+++ scripts/generate_symbolic_link.sh
@@ -6,7 +6,6 @@ cd $THIS_DIR
 ln -snfv "$THIS_DIR/.Brewfile" "$HOME"
 ln -snfv "$THIS_DIR/.Brewfile.lock.json" "$HOME"
 ln -snfv "$THIS_DIR/Makefile" "$HOME"
-# NOTE: シンボリックリンクでは設定が反映されなかったため、ハードリンクに変更
-ln -v "$THIS_DIR/karabiner/karabiner.json" "$HOME/.config/karabiner"
+ln -snfv "$THIS_DIR/karabiner/karabiner.json" "$HOME/.config/karabiner"
 
 cd ~
