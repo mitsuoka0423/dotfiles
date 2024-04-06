ceafbdc802ddbd2160c4166c811ba6bea158bf01
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2024-04-06 20:41:53 +0900

    feat: android

---
+++ scripts/install/cmdline-tools.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+curl -L https://dl.google.com/android/repository/commandlinetools-mac-8512546_latest.zip -o $HOME/Downloads/commandlinetools-mac-8512546_latest.zip
+unzip -o "$HOME/Downloads/commandlinetools-mac-8512546_latest.zip" -d "$HOME/Downloads/cmdline-tools"
+
